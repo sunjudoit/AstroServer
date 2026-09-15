890d110f419b53a36540f6bfbbbd2138f8bd3fb6
sunjudoit <lovelysupergirl314@gmail.com>
2026-09-15 12:19:09 +0800

    Add server waiting status message

--- AstroServer/AstroServer/Program.cs
+++ AstroServer/AstroServer/Program.cs
@@ -1,4 +1,6 @@
 
+using AstroMath;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
@@ -7,8 +9,16 @@ builder.Services.AddGrpc();
 // build app
 var app = builder.Build();
 
+// Test AstroMath.dll
+//AstroCalculations calculations = new AstroCalculations();
+
+//Console.WriteLine("Star Velocity: " +
+    //calculations.StarVelocity(660, 656));
+
 
 //  Map the gRPC service
 app.MapGrpcService<AstroServer.Services.AstroServer>();
+
+Console.WriteLine("AstroServer is running and Waiting for requests.");
 
 app.Run();
